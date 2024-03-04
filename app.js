9ffa7b637ea7eeed95e20b3fc2fe6ef21326670d
Quidenk <anhb2113304@student.ctu.edu.vn>
2024-03-05 01:23:28 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
@@ -17,15 +17,15 @@ app.get("/", (req, res) => {
 });
 
 
-//handle 404 response
-// app.use((req, res, next)=>{
-//     return next(new ApiError(404, "Resource not found"));
-// });
+// handle 404 response
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+});
 
-// app.use((err, req, res, next)=>{
-//     return res.status(err.statusCode || 500).json({
-//         message : err.message || "Internal Server Error",
-//     });
-// });
+app.use((err, req, res, next)=>{
+    return res.status(err.statusCode || 500).json({
+        message : err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
